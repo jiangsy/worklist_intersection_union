36095866b6a6007099389945282d0600b97da842
Shengyi Jiang <shengyi.jiang@outlook.com>
2022-03-05 14:17:14 +0800

    optimize imports and remove old code

--- implementation/deprecated/TestInfer.hs
+++ implementation/deprecated/TestInfer.hs
@@ -1,7 +1,9 @@
 {-# LANGUAGE FlexibleInstances, MultiWayIf, ScopedTypeVariables #-}
 module TestInfer where
 
-import Control.Monad ( liftM2 ) 
+import Control.Monad (liftM,liftM2,liftM3)
+import Data.Functor
+import Data.Time.Clock ( UTCTime(utctDayTime), getCurrentTime )
 import Test.QuickCheck
     ( chooseInt,
       elements,
@@ -11,10 +13,10 @@ import Test.QuickCheck
       verboseCheck,
       Arbitrary(arbitrary),
       Gen )
-import Test.QuickCheck.Property
-    ( Result(reason), failed, succeeded )
-import System.Random
-    ( mkStdGen, Random(random, randomR), RandomGen(genWord32) ) 
+import Test.QuickCheck.Property (succeeded, failed, reason, Result)
+import System.IO.Unsafe
+import System.Random (StdGen, mkStdGen, random, randomR, randomRs, RandomGen)
+import System.Random.Stateful (RandomGen(genWord32))
 
 
 import LazyDef (Typ(..), Work(..))
@@ -67,7 +69,45 @@ cumProbs :: [Float]
 cumProbs = 
     let cumProbs = 0.0 : scanl1 (+) probs in
         if abs(1.0 - last cumProbs) < 0.01 then cumProbs else error "Error: all probs should sum to 1.0!"
-        
+
+-- abstractHelper :: RandomGen g => g -> Typ -> [Typ] -> (Typ, g)
+-- abstractHelper gen TInt typLs =
+--     let (x, nextGen) = random gen in
+--         if (x :: Float) < 0.5 then (TInt, nextGen) else
+--             let (t, nextGen') = getRandomElement nextGen typLs
+--                 in (t, nextGen')
+-- abstractHelper gen TBool typLs =
+--     let (x, nextGen) = random gen in
+--         if (x :: Float) < 0.5 then (TBool, nextGen) else
+--             let (t, nextGen') = getRandomElement nextGen typLs
+--                 in (t, nextGen')
+-- abstractHelper gen (TArrow t1 t2) typLs =
+--     let (x, nextGen) = random gen in
+--         if | (x :: Float) > head cumProbs && (x :: Float) > cumProbs !! 1->
+--                 let (t1', nextGen) = abstractHelper gen t1 typLs in
+--                     let (t2', nextGen') = abstractHelper nextGen t2 typLs in
+--                         (TArrow t1' t2', nextGen')
+--            | (x :: Float) > cumProbs !! 1 && (x :: Float) > cumProbs !! 2 ->
+--                let (tvar, nextGen) = getRandomElement gen typLs in
+--                    let (t2', nextGen') = abstractHelper nextGen t2 typLs in
+--                     (TArrow tvar t2', nextGen')
+--            | (x :: Float) > cumProbs !! 1 && (x :: Float) > cumProbs !! 2 ->
+--                let (tvar, nextGen) = getRandomElement gen typLs in
+--                    let (t1', nextGen') = abstractHelper nextGen t1 typLs in
+--                     (TArrow t1' tvar, nextGen')
+--            | (x :: Float) > cumProbs !! 2 && (x :: Float) > cumProbs !! 3 ->
+--                let (tvar1, nextGen) = getRandomElement gen typLs in
+--                    let (tvar2, nextGen') = getRandomElement nextGen typLs in 
+--                        (TArrow tvar1 tvar2, gen)
+--            | (x :: Float) > cumProbs !! 3 && (x :: Float) > cumProbs !! 4 ->
+--                 (TForall $ \x -> TArrow (fst $ abstractHelper nextGen t1 (x:typLs)) t2, nextGen)
+--            | (x :: Float) > cumProbs !! 4 && (x :: Float) > cumProbs !! 5 ->
+--                 (TForall $ \x -> TArrow t1 (fst $ abstractHelper nextGen t2 (x:typLs)), nextGen)
+--            | (x :: Float) > 0.5 -> (TArrow t1 t2, gen)
+--            | otherwise -> (TArrow t1 t2, gen)
+-- abstractHelper gen (TVar _) typLs = error "Bug: Wellformed type should not contain var!"
+-- abstractHelper gen (TForall tt) typLs = (TForall (\x -> fst $ abstractHelper gen (tt x) (x:typLs)), gen)
+
 -- abstract :: Typ -> Int -> Typ
 -- abstract typ seed = fst $ abstractHelper (mkStdGen seed) typ []
 maxRandomSeedUsed :: Typ -> Int
@@ -99,6 +139,14 @@ getRandomFloat seed  =
         gen = mkStdGen seed
         x = fst $ random gen
         nextSeed = fromIntegral $ fst $ genWord32 gen
+
+-- avoid call-by-need optimization, otherwise calling getSeedFromTime will get the same result
+getSeedFromTime :: Int -> Int
+getSeedFromTime _ = round $ 1e6 * unsafePerformIO (getCurrentTime <&> (fromRational . toRational . utctDayTime))
+
+getSeedsFromTime :: [Int]
+getSeedsFromTime = getSeedsFromTimeHelper 0
+    where getSeedsFromTimeHelper n = getSeedFromTime n : getSeedsFromTimeHelper (n + 1)
 
 abstractHelper :: Int -> Typ -> [Typ] -> (Typ, Int)
 abstractHelper seed TInt typLs =
@@ -151,11 +199,54 @@ abstract :: Typ -> Int -> Typ
 abstract typ seed = fst $ abstractHelper seed typ []
 
 
+-- https://www.seas.upenn.edu/~cis552/12fa/lectures/stub/QuickCheck.html
+-- https://stackoverflow.com/questions/17615138/haskell-quickcheck-to-generate-and-test-rose-trees
+-- https://www.cse.chalmers.se/~rjmh/QuickCheck/manual_body.html
+monoTypGen :: Gen Typ
+monoTypGen = sized monoNTypGen
+    where monoNTypGen 0 = elements [TBool, TInt]
+          monoNTypGen n = frequency [(4, liftM2 TArrow (monoNTypGen (n - 1)) (monoNTypGen (n - 1))),
+                                     (1, elements [TBool, TInt])]
+
 monoNTypGen :: Int -> Gen Typ
 monoNTypGen 0 = elements [TBool, TInt]
 monoNTypGen n = oneof [liftM2 TArrow (monoNTypGen (n - 1)) (monoNTypGen (n - 1)),
                         elements [TBool, TInt]
                     ]
+
+polyTypGen :: Typ -> Gen Typ
+polyTypGen = polyTypGenHelper []
+    where
+        polyTypGenHelper typLs TInt =
+            do
+                t <- elements typLs
+                elements [TInt, t]
+        polyTypGenHelper typLs TBool =
+            do
+                t <- elements typLs
+                elements [TBool, t]
+        polyTypGenHelper typLs (TArrow t1 t2) =
+            if null typLs then
+                do
+                    polyt1 <- polyTypGenHelper typLs t1
+                    polyt2 <- polyTypGenHelper typLs t2
+                    let ta = TArrow polyt1 polyt2
+                    elements [ta]
+            else
+                do
+                    tvar1 <- elements typLs
+                    tvar2 <- elements typLs
+                    polyt1 <- polyTypGenHelper typLs t1
+                    polyt2 <- polyTypGenHelper typLs t2
+                    let ta = TArrow polyt1 polyt2
+                    let tb = TArrow tvar1 polyt2
+                    let tc = TArrow polyt1 tvar2
+                    let td = TArrow tvar1 tvar2
+                    let te = TForall (\x -> TArrow x x)
+                    let tf = \x -> polyTypGenHelper (x:typLs) t1
+                    elements [ta, tb, tc, td, te]
+        polyTypGenHelper typLs t = error $ "Bug: Wellformed mono type should not contain " ++ show t ++ "!"
+
 
 instance Arbitrary Typ where
     arbitrary = 
@@ -176,7 +267,7 @@ instance {-# OVERLAPPING #-} Arbitrary [Work] where
                           (2, return [Sub (abstract t1 seed1) (abstract t1 (seed1+1))]),
                           (2, return [Sub (abstract t1 seed1) (abstract t2 (seed1+1))])]
 
-test1 = chkAndShow [Sub (TArrow TInt (TArrow TInt TBool)) (TForall (\t -> (TArrow t (TArrow TInt t))))]
+test1 = chkAndShow [Sub (TArrow TInt (TArrow TInt TBool )) (TForall (\t -> (TArrow t (TArrow TInt t))))]
 
 wl = [Sub t12 t13]
 wl' = map adaptWorkLStoS wl 
