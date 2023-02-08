1765820a0ddfb5a75f4149bd4921e71d71b6d1ba
Shengyi Jiang <shengyi.jiang@outlook.com>
2023-02-08 10:56:58 +0800

    update decl defs to add exprs

--- proof/iu_system/decl/ln_inf_extra.v
+++ proof/iu_system/decl/ln_inf_extra.v
@@ -2,42 +2,39 @@ Require Import decl.ott.
 Require Export decl.notations.
 
 
-
-Lemma ld_type_subst_open_comm : forall x t1 A t2,
-  lc_ld_type t1 -> 
-  x `notin` fv_ld_type t2 -> 
-  ([t1 /ᵈ x] A) ^^ᵈ t2 = [t1 /ᵈ x] A ^^ᵈ t2.
+Lemma dtyp_subst_open_comm : forall X T1 T2 T3,
+  lc_dtyp T2 -> 
+  X `notin` ftv_in_dtyp T3 -> 
+  ([T2 /ᵈ X] T1) ^^ᵈ T3 = [T2 /ᵈ X] T1 ^^ᵈ T3.
 Proof.
   intros.
-  rewrite subst_ld_type_open_ld_type_wrt_ld_type.
-  rewrite (subst_ld_type_fresh_eq t2).
-  all: easy.
+  rewrite dsubst_tv_in_dtyp_open_dtyp_wrt_dtyp; auto.
+  rewrite (dsubst_tv_in_dtyp_fresh_eq T3); auto.
 Qed.
 
 
-Lemma ld_type_open_r_close_l : forall t1 t2 x
-  , x `notin` fv_ld_type t2
-  -> t1 = open_ld_type_wrt_ld_type t2 `ᵈ x -> close_ld_type_wrt_ld_type x t1 = t2.
+Lemma dtyp_open_r_close_l : forall T1 T2 X
+  , X `notin` ftv_in_dtyp T2
+  -> T1 = open_dtyp_wrt_dtyp T2 `ᵈ X -> close_dtyp_wrt_dtyp X T1 = T2.
 Proof.
   intros * Fr H.
-  assert (close_ld_type_wrt_ld_type x t1 = close_ld_type_wrt_ld_type x (open_ld_type_wrt_ld_type t2 `ᵈ x)) by now rewrite H.
-  now rewrite close_ld_type_wrt_ld_type_open_ld_type_wrt_ld_type in H0.
+  assert (close_dtyp_wrt_dtyp X T1 = close_dtyp_wrt_dtyp X (open_dtyp_wrt_dtyp T2 `ᵈ X)) by now rewrite H.
+  now rewrite close_dtyp_wrt_dtyp_open_dtyp_wrt_dtyp in H0.
 Qed.
 
 
-Lemma close_ld_type_notin_rec : forall x e n,
-    x `notin` fv_ld_type (close_ld_type_wrt_ld_type_rec n x e).
+Lemma close_dtyp_notin_rec : forall X e n,
+    X `notin` ftv_in_dtyp (close_dtyp_wrt_dtyp_rec n X e).
 Proof.
   intros until e.
-
   induction e; simpl; intros; auto.
   - destruct (lt_ge_dec n n0); auto.
   - unfold eq_dec. destruct EqDec_eq_of_X; auto.
 Qed.
 
 
-Lemma close_ld_type_notin : forall x e,
-    x `notin` fv_ld_type (close_ld_type_wrt_ld_type x e).
+Lemma close_ld_type_notin : forall X e,
+    X `notin` ftv_in_dtyp (close_dtyp_wrt_dtyp X e).
 Proof.
-  intros. apply close_ld_type_notin_rec.
+  intros. apply close_dtyp_notin_rec.
 Qed.
--- proof/iu_system/decl/notations.v
+++ proof/iu_system/decl/notations.v
@@ -3,65 +3,64 @@ Require Export Coq.Unicode.Utf8.
 Require Export decl.ott.
 Require Export decl.ln_inf.
 
-Notation "⊢ G" :=
-  (ld_wf_context G)
-    (at level 65, no associativity) : ld_type_scope.
+Notation "⊢ E" :=
+  (dwf_env E)
+    (at level 65, no associativity) : type_scope.
 
-Notation "G ⊢ t" :=
-  (ld_wf_type G t)
-    (at level 65, no associativity) : ld_type_scope.
+Notation "E ⊢ T" :=
+  (dwf_typ E T)
+    (at level 65, no associativity) : type_scope.
 
-Notation "G ⊢ t1 <: t2" :=
-  (ld_sub G t1 t2)
-    (at level 65, t1 at next level, no associativity) : ld_type_scope.
+Notation "E ⊢ S <: T" :=
+  (dsub E S T)
+    (at level 65, S at next level, no associativity) : type_scope.
 
-Notation "x ∈ G" := (ld_in_context x G)
-  (at level 65, no associativity) : type_ld_type_scopescope.
+Notation "E ⊢ e <= T" :=
+    (d_check E e T) 
+      (at level 65, e at next level, no associativity) : 
+      type_scope.
 
-Notation "`ᵈ x" := (ld_t_var_f x)
-  (at level 0, x at level 0, no associativity) : ld_type_scope.
-Notation "↑ n" := (ld_t_var_b n)
-  (at level 0, n at level 0, no associativity) : ld_type_scope.
+Notation "X ∈ E" := (binds X (dbind_tvar_empty))
+  (at level 65, no associativity) : type_scope.
 
-
-Notation "[ t' /ᵈ x ] t" :=
-  (subst_ld_type t' x t)
-    ( at level 49, t' at level 50, x at level 0
-    , right associativity) : ld_type_scope.
-
-Notation "t ^ᵈ x" := (open_ld_type_wrt_ld_type t (ld_t_var_f x))
-  (at level 48, left associativity) : ld_type_scope.
-
-Notation "t1 ^^ᵈ t2" := (open_ld_type_wrt_ld_type t1 t2)
-  (at level 48, left associativity) : ld_type_scope.
+Notation "~ X ∈ E" := (binds X (dbind_stvar_empty))
+  (at level 65, no associativity) : type_scope.
 
 
-Declare Scope ld_context_scope.
-Delimit Scope ld_context_scope with ld_context.
-Bind Scope ld_context_scope with ld_context.
+Notation "`ᵈ X" := (dtyp_var_f X)
+  (at level 0, X at level 0, no associativity) : type_scope.
+Notation "↑ n" := (dtyp_var_b n)
+  (at level 0, n at level 0, no associativity) : type_scope.
+
+Notation "[ T' /ᵈ X ] T" :=
+  (dsubst_tv_in_dtyp T' X T)
+    ( at level 49, T' at level 50, X at level 0
+    , right associativity) : type_scope.
+
+Notation "T ^ᵈ X" := (open_dtyp_wrt_dtyp T (dtyp_var_f X))
+  (at level 48, left associativity) : type_scope.
+
+Notation "T1 ^^ᵈ T2" := (open_dtyp_wrt_dtyp T1 T2)
+  (at level 48, left associativity) : type_scope.
+
+
+(* Declare Scope decl_context_scope.
+Delimit Scope decl_context_scope with decl_context.
+Bind Scope decl_context_scope with decl_context.
 
 Notation "G , x" :=
-  (ld_ctx_cons G x)
-    (at level 58, x at level 0, left associativity) : ld_context_scope.
+  (decl_ctx_cons G x)
+    (at level 58, x at level 0, left associativity) : decl_context_scope.
 
 Reserved Notation "G1 ,, G2"
   (at level 58, left associativity).
 
-Fixpoint ld_ctx_app (G1 G2 : ld_context) : ld_context :=
+Fixpoint decl_ctx_app (G1 G2 : decl_context) : decl_context :=
   match G2 with
-  | ld_ctx_nil => G1
+  | decl_ctx_nil => G1
   | G2', x => G1 ,, G2' , x
-  end%ld_context
-where "G1 ,, G2" := (ld_ctx_app G1 G2) : ld_context_scope.
+  | decl_ctx_cons_sv G2' sx => decl_ctx_cons_sv (G1 ,, G2') sx
+  end%decl_context
+where "G1 ,, G2" := (decl_ctx_app G1 G2) : decl_context_scope.
 
-
-Fixpoint ld_wl_app (Γ1 Γ2 : ld_worklist) : ld_worklist :=
-  match Γ2 with 
-  | ld_wl_nil => Γ1 
-  | ld_wl_cons_tv Γ2' x => ld_wl_cons_tv (ld_wl_app Γ1 Γ2') x
-  | ld_wl_cons_w Γ2' w => ld_wl_cons_w (ld_wl_app Γ1 Γ2') w
-  end.
-
-
-Open Scope ld_type_scope.
-Open Scope ld_context_scope.
+Open Scope decl_context_scope. *)
