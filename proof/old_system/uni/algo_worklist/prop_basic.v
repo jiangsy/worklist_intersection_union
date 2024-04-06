e8c38a70678abe5b1f986b8a4d828e673ab6dec3
xsnow <xsnow@outlook.com>
2024-04-06 08:18:51 +0800

    Comment incomplete proofs

--- proof/old_system/uni/algo_worklist/prop_basic.v
+++ proof/old_system/uni/algo_worklist/prop_basic.v
@@ -110,8 +110,8 @@ Proof.
     induction* HE.
     gen Σ1 A1 A2.
     induction Σ2; intros; cbn in *.
-    + inverts* HE.
-  - introv HB HT. clear a_wf_body_var_binds_another.
+    (* + inverts* HE. *)
+  (* - introv HB HT. clear a_wf_body_var_binds_another. *)
 Admitted.
 
 Lemma a_wf_exp_var_binds_another_cons : forall Σ1 x e A1 A2,
