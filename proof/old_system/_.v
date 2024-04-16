d47c7f23327b95badfa2967fdfa8b2f425c2d3bd
Shengyi Jiang <shengyi.jiang@outlook.com>
2024-04-16 15:40:22 +0800

    - clean _.v

--- proof/old_system/_.v
+++ proof/old_system/_.v
@@ -1,13 +1,6 @@
-Require Import ltac_utils.
-Require Import uni.def_ott.
-Require Import uni.decl.def_extra.
-Require Import uni.notations.
-Require Import uni.prop_basic.
+
 Require Import uni.decl.prop_typing.
 Require Import uni.decl_worklist.prop_equiv.
-Require Import uni.algo_worklist.transfer.
-Require Import uni.algo_worklist.def_extra.
-
 Require Import uni.algo_worklist.prop_soundness.
 Require Import uni.algo_worklist.prop_completeness.
 (* Require Import uni.algo_worklist.prop_decidability. *)
