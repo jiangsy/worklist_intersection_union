d305c85e1469d7332062e372b946facc822f5411
Shengyi Jiang <shengyi.jiang@outlook.com>
2024-05-21 00:56:36 +0800

    - add missing cases of fvar/ftvar/**subst

--- implementation/src/Alg.hs
+++ implementation/src/Alg.hs
@@ -91,46 +91,51 @@ eesubst s e RcdNil = RcdNil
 eesubst s e (RcdCons l1 e1 e2) = RcdCons l1 (eesubst s e e1) (eesubst s e e2)
 eesubst s e (RcdProj e1 l1) = RcdProj (eesubst s e e1) l1
 
-etsubst :: String -> Typ -> Exp -> Exp
-etsubst e s (Lam x b) = Lam x (etsubst e s b)
-etsubst e s (App e1 e2) = App (etsubst e s e1) (etsubst e s e2)
-etsubst e s (Ann e1 t) = Ann (etsubst e s e1) (ttsubst e s t)
-etsubst e s (TApp e1 t) = TApp (etsubst e s e1) (ttsubst e s t)
-etsubst e s (TAbs x e1) = TAbs x (etsubst e s e1)
-etsubst e s (Cons e1 e2) = Cons (etsubst e s e1) (etsubst e s e2)
-etsubst e s (Case e1 e2 e3) = Case (etsubst e s e1) (etsubst e s e2) (etsubst e s e3)
-etsubst e s (Fix e1) = Fix (etsubst e s e1)
-etsubst e s (Let x e1 e2) = Let x (etsubst e s e1) (etsubst e s e2)
-etsubst e s (LetA x t e1 e2) = LetA x (ttsubst e s t) (etsubst e s e1) (etsubst e s e2)
-etsubst e s RcdNil = RcdNil
-etsubst e s (RcdCons l1 e1 e2) = RcdCons l1 (etsubst e s e1) (etsubst e s e2)
-etsubst e s (RcdProj e1 l1) = RcdProj (etsubst e s e1) l1
-
 ttsubst :: String -> Typ -> Typ -> Typ
 ttsubst _ _ TInt = TInt
 ttsubst _ _ TBool = TBool
 ttsubst _ _ TTop = TTop
 ttsubst _ _ TBot = TBot
-ttsubst s t (TVar x)
-  | s == x = t
-  | otherwise = TVar x
-ttsubst s t (TAll a c)
-  | s == a = TAll a c
-  | otherwise = TAll a (ttsubst s t c)
-ttsubst s t (TArr t1 t2) =
-  TArr (ttsubst s t t1) (ttsubst s t t2)
-ttsubst s t (TList t1) = TList (ttsubst s t t1)
+ttsubst sa st (TVar a)
+  | sa == a = st
+  | otherwise = TVar a
+ttsubst sa st (TAll a t)
+  | sa == a = TAll a t
+  | otherwise = TAll a (ttsubst sa st t)
+ttsubst sa st (TArr t1 t2) =
+  TArr (ttsubst sa st t1) (ttsubst sa st t2)
+ttsubst sa st (TList t) = TList (ttsubst sa st t)
+ttsubst sa st (TIntersection t1 t2) =
+  TIntersection (ttsubst sa st t1) (ttsubst sa st t2)
+ttsubst sa st (TUnion t1 t2) =
+  TUnion (ttsubst sa st t1) (ttsubst sa st t2)
+ttsubst _ _ (TLabel l) = TLabel l
+
+etsubst :: String -> Typ -> Exp -> Exp
+etsubst sa st (Lam x b) = Lam x (etsubst sa st b)
+etsubst sa st (App e1 e2) = App (etsubst sa st e1) (etsubst sa st e2)
+etsubst sa st (Ann e1 t) = Ann (etsubst sa st e1) (ttsubst sa st t)
+etsubst sa st (TApp e1 t) = TApp (etsubst sa st e1) (ttsubst sa st t)
+etsubst sa st (TAbs x e1) = TAbs x (etsubst sa st e1)
+etsubst sa st (Cons e1 e2) = Cons (etsubst sa st e1) (etsubst sa st e2)
+etsubst sa st (Case e1 e2 e3) = Case (etsubst sa st e1) (etsubst sa st e2) (etsubst sa st e3)
+etsubst sa st (Fix e1) = Fix (etsubst sa st e1)
+etsubst sa st (Let x e1 e2) = Let x (etsubst sa st e1) (etsubst sa st e2)
+etsubst sa st (LetA x t e1 e2) = LetA x (ttsubst sa st t) (etsubst sa st e1) (etsubst sa st e2)
+etsubst sa st RcdNil = RcdNil
+etsubst sa st (RcdCons l1 e1 e2) = RcdCons l1 (etsubst sa st e1) (etsubst sa st e2)
+etsubst sa st (RcdProj e1 l1) = RcdProj (etsubst sa st e1) l1
 
 ctsubst :: String -> Typ -> Judgment -> Judgment
-ctsubst s t (Sub a b) = Sub (ttsubst s t a) (ttsubst s t b)
-ctsubst s t (Chk e a) = Chk (etsubst s t e) (ttsubst s t a)
-ctsubst s t (Inf e f) = Inf (etsubst s t e) (ctsubst s t . f)
-ctsubst s t (InfAbs t1 f) = InfAbs (ttsubst s t t1) (\a b -> ctsubst s t (f a b))
-ctsubst s t (InfApp t1 t2 e f) = InfApp (ttsubst s t t1) (ttsubst s t t2) (etsubst s t e) (ctsubst s t . f)
-ctsubst s t (InfTApp t1 t2 f) = InfTApp (ttsubst s t t1) (ttsubst s t t2) (ctsubst s t . f)
-ctsubst s t (CaseChk e a b) = CaseChk (etsubst s t e) (ttsubst s t a) (ttsubst s t b)
-ctsubst s t (CaseInf t1 e e1 f) = CaseInf (ttsubst s t t1) (etsubst s t e) (etsubst s t e1) (ctsubst s t . f)
-ctsubst s t (ConsInf t1 e f) = ConsInf (ttsubst s t t1) (etsubst s t e) (ctsubst s t . f)
+ctsubst sa st (Sub a b) = Sub (ttsubst sa st a) (ttsubst sa st b)
+ctsubst sa st (Chk e a) = Chk (etsubst sa st e) (ttsubst sa st a)
+ctsubst sa st (Inf e f) = Inf (etsubst sa st e) (ctsubst sa st . f)
+ctsubst sa st (InfAbs t1 f) = InfAbs (ttsubst sa st t1) (\a b -> ctsubst sa st (f a b))
+ctsubst sa st (InfApp t1 t2 e f) = InfApp (ttsubst sa st t1) (ttsubst sa st t2) (etsubst sa st e) (ctsubst sa st . f)
+ctsubst sa st (InfTApp t1 t2 f) = InfTApp (ttsubst sa st t1) (ttsubst sa st t2) (ctsubst sa st . f)
+ctsubst sa st (CaseChk e a b) = CaseChk (etsubst sa st e) (ttsubst sa st a) (ttsubst sa st b)
+ctsubst sa st (CaseInf t1 e e1 f) = CaseInf (ttsubst sa st t1) (etsubst sa st e) (etsubst sa st e1) (ctsubst sa st . f)
+ctsubst sa st (ConsInf t1 e f) = ConsInf (ttsubst sa st t1) (etsubst sa st e) (ctsubst sa st . f)
 ctsubst _ _ End = End
 
 ftvarInTyp :: Typ -> [String]
@@ -157,8 +162,36 @@ tvarInTyp (TList t1) = tvarInTyp t1
 tvarInTyp (TIntersection t1 t2) = tvarInTyp t1 `union` tvarInTyp t2
 tvarInTyp (TUnion t1 t2) = tvarInTyp t1 `union` tvarInTyp t2
 
-genSplit :: [Char] -> ([Char], [Char])
-genSplit x = (x ++ "1", x ++ "2")
+tvarInExp :: Exp -> [String]
+tvarInExp (Var _) = []
+tvarInExp (ILit _) = []
+tvarInExp (BLit _) = []
+tvarInExp (Lam x b) = tvarInExp b
+tvarInExp (App e1 e2) = tvarInExp e1 `union` tvarInExp e2
+tvarInExp (Ann e t) = tvarInTyp t `union` tvarInExp e
+tvarInExp (TApp e t) = tvarInTyp t `union` tvarInExp e
+tvarInExp (TAbs x e) = [x] `union` tvarInExp e
+tvarInExp Nil = []
+tvarInExp (Cons e1 e2) = tvarInExp e1 `union` tvarInExp e2
+tvarInExp (Case e1 e2 e3) = tvarInExp e1 `union` tvarInExp e2 `union` tvarInExp e3
+tvarInExp (Fix e) = tvarInExp e
+tvarInExp (Let x e1 e2) = tvarInExp e1 `union` tvarInExp e2
+tvarInExp (LetA x t e1 e2) = tvarInTyp t `union` tvarInExp e1 `union` tvarInExp e2
+tvarInExp RcdNil = []
+tvarInExp (RcdCons _ e1 e2) = tvarInExp e1 `union` tvarInExp e2
+tvarInExp (RcdProj e _) = tvarInExp e
+
+tvarInJug :: Judgment -> [String]
+tvarInJug (Sub t1 t2) = tvarInTyp t1 `union` tvarInTyp t2
+tvarInJug (Chk e t) = tvarInExp e `union` tvarInTyp t
+tvarInJug (Inf e f) = tvarInExp e `union` tvarInJug (f TTop)
+tvarInJug (InfAbs t f) = tvarInTyp t `union` tvarInJug (f TTop TTop)
+tvarInJug (InfApp t1 t2 e f) = tvarInExp e `union` tvarInTyp t1 `union` tvarInTyp t2 `union` tvarInJug (f TTop)
+tvarInJug (InfTApp t1 t2 f) = tvarInTyp t1 `union` tvarInTyp t2 `union` tvarInJug (f TTop)
+tvarInJug (CaseChk e t1 t2) = tvarInExp e `union` tvarInTyp t1 `union` tvarInTyp t2
+tvarInJug (CaseInf t e1 e2 f) = tvarInTyp t `union` tvarInExp e1 `union` tvarInExp e2 `union` tvarInJug (f TTop)
+tvarInJug (ConsInf t e f) = tvarInTyp t `union` tvarInExp e `union` tvarInJug (f TTop)
+tvarInJug End = []
 
 findVar :: String -> Worklist -> Maybe Typ
 findVar x (WVar y a : w)
@@ -170,37 +203,61 @@ findVar _ [] = Nothing
 tvarInTyps = foldl union [] . map tvarInTyp
 
 pickNewTVar :: [Work] -> [Typ] -> [Char]
-pickNewTVar ws ts = genFreshTVar (wvars `union` tvarInTyps ts)
+pickNewTVar ws ts = genFreshTVar (wtvars `union` tvarInTyps ts)
   where
-    wvars =
+    wtvars =
       concatMap
         ( \case
-            WTVar v _ -> [v]
-            _ -> []
+            WTVar a _ -> [a]
+            WVar x t -> tvarInTyp t
+            WJug c -> tvarInJug c
         )
         ws
 
-expVar :: Exp -> [String]
-expVar (Var x) = [x]
-expVar (Lam x b) = x : expVar b
-expVar (App e1 e2) = expVar e1 ++ expVar e2
-expVar (Cons e1 e2) = expVar e1 ++ expVar e2
-expVar (Case e1 e2 e3) = expVar e1 ++ expVar e2 ++ expVar e3
-expVar (Fix e) = expVar e
-expVar _ = []
+varInExp :: Exp -> [String]
+varInExp (Var x) = [x]
+varInExp (ILit _) = []
+varInExp (BLit _) = []
+varInExp (Lam x b) = x : varInExp b
+varInExp (App e1 e2) = varInExp e1 `union` varInExp e2
+varInExp (Ann e _) = varInExp e
+varInExp (TApp e _) = varInExp e
+varInExp (TAbs x e) = varInExp e
+varInExp Nil = []
+varInExp (Cons e1 e2) = varInExp e1 `union` varInExp e2
+varInExp (Case e1 e2 e3) = varInExp e1 `union` varInExp e2 `union` varInExp e3
+varInExp (Fix e) = varInExp e
+varInExp (Let x e1 e2) = x : varInExp e1 ++ varInExp e2
+varInExp (LetA x _ e1 e2) = x : varInExp e1 ++ varInExp e2
+varInExp RcdNil = []
+varInExp (RcdCons _ e1 e2) = varInExp e1 ++ varInExp e2
+varInExp (RcdProj e _) = varInExp e
 
-pickNewVar :: Foldable t => Exp -> t Work -> String
-pickNewVar e w = fromJust $ find (`notElem` wvars) bvarsupply
+varInJug :: Judgment -> [String]
+varInJug (Sub t1 t2) = []
+varInJug (Chk e t) = varInExp e
+varInJug (Inf e f) = varInExp e `union` varInJug (f TTop)
+varInJug (InfAbs t f) = varInJug (f TTop TTop)
+varInJug (InfApp t1 t2 e f) = varInExp e `union` varInJug (f TTop)
+varInJug (InfTApp t1 t2 f) = varInJug (f TTop)
+varInJug (CaseChk e t1 t2) = varInExp e
+varInJug (CaseInf t e1 e2 f) = varInExp e1 `union` varInExp e2 `union` varInJug (f TTop)
+varInJug (ConsInf t e f) = varInExp e `union` varInJug (f TTop)
+varInJug End = []
+
+varInExps = foldl union [] . map varInExp
+
+pickNewVar :: [Work] -> [Exp] -> String
+pickNewVar ws es = genFreshVar (wvars `union` varInExps es)
   where
     wvars =
       concatMap
         ( \case
-            WVar v _ -> [v]
+            WVar a _ -> [a]
+            WJug c -> varInJug c
             _ -> []
         )
-        w
-        ++ expVar e
-    bvarsupply = "x" : "y" : [xy : show @Integer n | n <- [1 .. 100], xy <- ['x', 'y']]
+        ws
 
 findTVar :: [Work] -> String -> TBind
 findTVar w a = head [t | WTVar a' t <- w, a == a']
@@ -263,8 +320,10 @@ bigStep info (WJug (Sub _ TTop) : ws) = bigStep (info ++ curInfo ws "≤⊤") ws
 bigStep info (WJug (Sub TBot _) : ws) = bigStep (info ++ curInfo ws "≤⊥") ws
 bigStep info (WJug (Sub TInt TInt) : ws) = bigStep (info ++ curInfo ws "≤Int") ws
 bigStep info (WJug (Sub TBool TBool) : ws) = bigStep (info ++ curInfo ws "≤Bool") ws
-bigStep info (WJug (Sub (TVar x) (TVar y)) : ws)
-  | x == y = bigStep (info ++ curInfo ws "≤TVar") ws
+bigStep info (WJug (Sub (TLabel l1) (TLabel l2)) : ws)
+  | l1 == l2 = bigStep (info ++ curInfo ws "≤Label") ws
+bigStep info (WJug (Sub (TVar a) (TVar b)) : ws)
+  | a == b = bigStep (info ++ curInfo ws "≤TVar") ws
 bigStep info (WJug (Sub (TArr t1 t2) (TArr t3 t4)) : ws) = bigStep (info ++ curInfo ws' "≤→") ws'
   where
     ws' = WJug (Sub t2 t4) : WJug (Sub t3 t1) : ws
@@ -316,68 +375,6 @@ bigStep info (WJug (Sub t1 (TUnion t21 t22)) : ws) = case bigStep (info ++ curIn
 -- New Subtyping
 --
 bigStep info _ = (False, info)
-
--- step (WJug (Sub (TIntersection t1 t2) t3) : w) = WJug (Sub t1 t3) : w
--- step (WJug (Sub (TIntersection t1 t2) t3) : w) = WJug (Sub t1 t3) : w
--- bigStep n info (Sub (TForall g) b : ws)             = bigStep (n+1) (info ++ curInfo ws' "SForallL") ws'
---                                                         where ws' = Sub (g (TVar (Right n))) b: WExVar n TBot TTop : ws
-
--- bigStep n info (Sub (TVar (Right i)) (TArrow a b) : ws)
---   | not $ mono (TArrow a b)                         = case carryBackInWL (TVar (Right i)) a1_a2 (Sub a1_a2 (TArrow a b) :
---                                                                                                  WExVar (n+1) TBot TTop :
---                                                                                                  WExVar n TBot TTop : ws)
---                                                            >>= updateBoundInWL (TVar (Right i)) (UB, a1_a2) of
---                                                         Left error -> (False, info++error)
---                                                         Right ws' -> bigStep (n+2) (info ++ curInfo ws' "SplitL") ws'
---                                                       where
---                                                         a1 = TVar (Right n)
---                                                         a2 = TVar $ Right (n + 1)
---                                                         a1_a2 = TArrow a1 a2
--- bigStep n info (Sub (TArrow a b) (TVar (Right i)) : ws)
---   | not $ mono (TArrow a b)                         = case carryBackInWL (TVar (Right i)) a1_a2 (Sub (TArrow a b) a1_a2 :
---                                                                                                  WExVar (n+1) TBot TTop :
---                                                                                                  WExVar n TBot TTop : ws)
---                                                           >>= updateBoundInWL (TVar (Right i)) (LB, a1_a2) of
---                                                         Left error -> (False, info ++ error)
---                                                         Right ws' -> bigStep (n+1) (info ++ curInfo ws' "SplitR") ws'
---                                                       where
---                                                         a1 = TVar (Right n)
---                                                         a2 = TVar $ Right (n + 1)
---                                                         a1_a2 = TArrow a1 a2
-
--- bigStep n info (Sub vi@(TVar (Right i)) vj@(TVar (Right j)) : ws)                                       -- 15 & 16
---   | prec ws vi vj                                   = case carryBackInWL vj vi ws >>= updateBoundInWL vj (UB, vi) of
---                                                         Left error -> (False, info++error)
---                                                         Right ws' -> bigStep n (info++curInfo ws' "SolveLExtVar") ws'
---   | prec ws vj vi                                   = case carryBackInWL vi vj ws >>= updateBoundInWL vi (UB, vj) of
---                                                         Left error -> (False, info ++ error)
---                                                         Right ws' -> bigStep n (info++curInfo ws' "SolveRExtVar") ws'
-
--- bigStep n info (Sub (TVar (Right i)) t : ws)                                        -- 11
---   | mono t                                          = case carryBackInWL (TVar (Right i)) t ws >>=
---                                                           updateBoundInWL (TVar (Right i)) (UB, t) of
---                                                         Left error -> (False, info ++ error)
---                                                         Right ws' -> bigStep n (info++curInfo ws' "SolveMonoL") ws'
-
--- bigStep n info (Sub t (TVar (Right i))  : ws)                                       -- 12
---   | mono t                                          = case carryBackInWL (TVar (Right i)) t ws >>=
---                                                           updateBoundInWL (TVar (Right i)) (LB, t) of
---                                                         Left error -> (False, info++error)
---                                                         Right ws' -> bigStep n (info++curInfo ws' "SolveMonoR") ws'
-
--- bigStep n info (Sub t1 (TIntersection t21 t22) : ws) = bigStep n (info ++ curInfo ws' "SIntersection") ws'
---                                                           where ws' = Sub t1 t21 : Sub t1 t22 : ws
--- bigStep n info (Sub (TIntersection t11 t12) t2 : ws) = case bigStep n info (Sub t11 t2 : ws) of
---                                                           (True, info) -> (True, info++curInfo (Sub t11 t2 : ws) "SIntersection")
---                                                           (False, info) -> bigStep n (info ++ curInfo (Sub t12 t2 : ws) "SIntersection") (Sub t12 t2 : ws)
-
--- bigStep n info (Sub (TUnion t11 t12) t2 : ws)        = bigStep n (info ++ curInfo ws' "SUnion") ws'
---                                                           where ws' = Sub t11 t2 : Sub t12 t2 : ws
--- bigStep n info (Sub t1 (TUnion t21 t22) : ws)        = case bigStep n info (Sub t1 t21 : ws) of
---                                                           (True, info) -> (True, info++curInfo (Sub t1 t21 : ws) "SUnion")
---                                                           (False, s) -> bigStep n (info++curInfo (Sub t1 t22 : ws) "SUnion") (Sub t1 t22 : ws)
-
--- bigStep _ info _                                     = (False, info)
 
 -- step :: Worklist -> Worklist
 -- -- Garbage collection
