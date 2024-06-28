8b6f4584853ffa9ef7dfb52a7bc303467941152a
Chen <i@cuichen.cc>
2024-06-28 14:14:54 +0800

    infabs_judge_size_wl_aworklist_subst

--- proof/uni/algo_worklist/prop_decidability.v
+++ proof/uni/algo_worklist/prop_decidability.v
@@ -3161,6 +3161,70 @@ Proof.
   eapply infabs_depth_wl_aworklist_subst in Hsubst; eauto.
 Qed.
 
+Lemma infabs_judge_size_wl_move_etvar_back : forall X Y Γ1 Γ2,
+  uniq (⌊ Y ~ᵃ ⬒ ;ᵃ Γ2 ⧺ X ~ᵃ ⬒ ;ᵃ Γ1 ⌋ᵃ) ->
+  infabs_judge_size_wl (Γ2 ⧺ X ~ᵃ ⬒ ;ᵃ Γ1) = infabs_judge_size_wl (Γ2 ⧺ X ~ᵃ ⬒ ;ᵃ Y ~ᵃ ⬒ ;ᵃ Γ1).
+Proof.
+  intros * Huniq. induction Γ2; intros; simpl in *; eauto.
+  dependent destruction Huniq. dependent destruction Huniq.
+  eapply IHΓ2; eauto.
+Qed.
+
+Lemma infabs_judge_size_conts_subst_typ_in_conts : forall c X A,
+  infabs_judge_size_conts ({A ᶜˢ/ₜ X} c) = infabs_judge_size_conts c
+with infabs_judge_size_contd_subst_typ_in_contd : forall c X A,
+  infabs_judge_size_contd ({A ᶜᵈ/ₜ X} c) = infabs_judge_size_contd c.
+Proof.
+  - clear infabs_judge_size_conts_subst_typ_in_conts.
+    intro c. induction c; simpl; intros *; eauto.
+  - clear infabs_judge_size_contd_subst_typ_in_contd.
+    intro c. induction c; simpl; intros *; eauto.
+Qed.
+
+Lemma infabs_judge_size_work_subst_typ_in_work : forall A X w,
+  infabs_judge_size_work w = infabs_judge_size_work ({A ʷ/ₜ X} w).
+Proof.
+  intros *; induction w; simpl in *; eauto;
+    try solve [erewrite infabs_judge_size_conts_subst_typ_in_conts; eauto];
+    try solve [erewrite infabs_judge_size_contd_subst_typ_in_contd; eauto].
+Qed.
+
+Lemma infabs_judge_size_wl_aworklist_subst : forall Γ2 X A Γ1 Γ1' Γ2',
+  ⊢ᵃʷ (Γ2 ⧺ X ~ᵃ ⬒ ;ᵃ Γ1) ->
+  aworklist_subst (Γ2 ⧺ X ~ᵃ ⬒ ;ᵃ Γ1) X A Γ1' Γ2' ->
+  X `notin` ftvar_in_typ A ->
+  ⌊ Γ2 ⧺ X ~ᵃ ⬒ ;ᵃ Γ1 ⌋ᵃ ᵗ⊢ᵃₘ A ->
+  infabs_judge_size_wl ({A ᵃʷ/ₜ X} Γ2' ⧺ Γ1') = infabs_judge_size_wl (Γ2 ⧺ X ~ᵃ ⬒ ;ᵃ Γ1).
+Proof.
+  intros Γ2. induction Γ2; intros * Hwf Hsubst Hnotin Hmono;
+    dependent destruction Hwf; dependent destruction Hsubst;
+    simpl in *; eauto; try solve [exfalso; eauto];
+    try solve [eapply IHΓ2; eauto;
+      try solve [eapply a_mono_typ_strengthen_var; eauto];
+      try solve [eapply a_mono_typ_strengthen_mtvar with (b := □%abind); eauto];
+      try solve [eapply a_mono_typ_strengthen_mtvar with (b := ⬒); eauto];
+      try solve [eapply a_mono_typ_strengthen_stvar; eauto]].
+  - eapply worklist_split_etvar_det in x. destruct x. subst.
+    rewrite infabs_judge_size_wl_move_etvar_back with (Y := X); eauto.
+    eapply IHΓ2 with (Γ1 := X ~ᵃ ⬒ ;ᵃ Γ1) (A := A); eauto.
+    eapply a_wf_wwl_move_etvar_back; eauto.
+    eapply a_mono_typ_move_etvar_back; eauto.
+    eapply a_wf_wwl_tvar_notin_remaining in Hwf; eauto.
+  - erewrite <- infabs_judge_size_work_subst_typ_in_work; eauto.
+Qed.
+
+Lemma infabs_judge_size_wl_aworklist_subst' : forall Γ X A Γ1' Γ2',
+  ⊢ᵃʷ Γ -> X ~ ⬒ ∈ᵃ ⌊ Γ ⌋ᵃ ->
+  aworklist_subst Γ X A Γ1' Γ2' ->
+  ⌊ Γ ⌋ᵃ ᵗ⊢ᵃₘ A ->
+  X `notin` ftvar_in_typ A ->
+  infabs_judge_size_wl ({A ᵃʷ/ₜ X} Γ2' ⧺ Γ1') = infabs_judge_size_wl Γ.
+Proof.
+  intros * Hwf Hbinds Hsubst Hmono Hnotin.
+  eapply awl_split_etvar in Hbinds as [Γ1 [Γ2 Hbinds]]. subst.
+  eapply infabs_judge_size_wl_aworklist_subst in Hsubst; eauto.
+Qed.
+
 Lemma a_wf_wl_red_decidable : forall me mj mt mtj ma maj ms mw ne Γ,
   ⊢ᵃʷₛ Γ ->
   exp_size_wl Γ ne -> ne < me ->
