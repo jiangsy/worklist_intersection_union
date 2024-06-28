7d16c91ce6bc4251657fbb1ebe8066865f0ce77b
Chen <i@cuichen.cc>
2024-06-28 14:10:00 +0800

    infabs_depth_wl_aworklist_subst

--- proof/uni/algo_worklist/prop_decidability.v
+++ proof/uni/algo_worklist/prop_decidability.v
@@ -3081,6 +3081,86 @@ Proof.
   eapply IHΓ2; eauto.
 Qed.
 
+Lemma infabs_depth_mono : forall Σ A,
+  Σ ᵗ⊢ᵃₘ A -> infabs_depth A <= 2.
+Proof.
+  intros * Hmono. induction Hmono; simpl in *; eauto.
+Qed.
+
+Lemma infabs_depth_subst_mono : forall Σ A B X,
+  Σ ᵗ⊢ᵃₘ A -> infabs_depth ({A ᵗ/ₜ X} B) <= infabs_depth B.
+Proof.
+  intros * Hmono. induction B; simpl in *; eauto; try lia.
+  destruct_eq_atom; simpl; eauto.
+  eapply infabs_depth_mono; eauto.
+Qed.
+
+Lemma infabs_depth_conts_subst_mono : forall c Σ A X,
+  Σ ᵗ⊢ᵃₘ A -> infabs_depth_conts ({A ᶜˢ/ₜ X} c) <= infabs_depth_conts c
+with infabs_depth_contd_subst_mono : forall c Σ A X,
+  Σ ᵗ⊢ᵃₘ A -> infabs_depth_contd ({A ᶜᵈ/ₜ X} c) <= infabs_depth_contd c.
+Proof.
+  - clear infabs_depth_conts_subst_mono.
+    intro c. induction c; simpl; intros * Hmono; eauto.
+    eapply infabs_depth_contd_subst_mono with (X := X) (c := cd) in Hmono. lia.
+  - clear infabs_depth_contd_subst_mono.
+    intro c. induction c; simpl; intros * Hmono; eauto.
+    + eapply infabs_depth_subst_mono with (X := X) (B := A) in Hmono as Hle; eauto.
+      eapply IHc with (X := X) in Hmono as Hle'. lia.
+    + eapply IHc with (X := X) in Hmono as Hle; eauto. lia.
+Qed.
+
+Lemma infabs_depth_work_subst_mono : forall Σ A X w,
+  Σ ᵗ⊢ᵃₘ A -> infabs_depth_work ({A ʷ/ₜ X} w) <= infabs_depth_work w.
+Proof.
+  intros * Hmono. induction w; simpl in *; eauto.
+  - eapply infabs_depth_subst_mono with (X := X) (B := A0) in Hmono as Hle.
+    eapply infabs_depth_contd_subst_mono with (X := X) (c := cd) in Hmono as Hle'.
+    lia.
+  - eapply infabs_depth_subst_mono with (X := X) (B := A2) in Hmono as Hle.
+    eapply infabs_depth_contd_subst_mono with (X := X) (c := cd) in Hmono as Hle'.
+    lia.
+  - eapply infabs_depth_contd_subst_mono with (X := X) (c := cd) in Hmono as Hle'.
+    lia.
+Qed.
+
+Lemma infabs_depth_wl_aworklist_subst : forall Γ2 X A Γ1 Γ1' Γ2',
+  ⊢ᵃʷ (Γ2 ⧺ X ~ᵃ ⬒ ;ᵃ Γ1) ->
+  aworklist_subst (Γ2 ⧺ X ~ᵃ ⬒ ;ᵃ Γ1) X A Γ1' Γ2' ->
+  X `notin` ftvar_in_typ A ->
+  ⌊ Γ2 ⧺ X ~ᵃ ⬒ ;ᵃ Γ1 ⌋ᵃ ᵗ⊢ᵃₘ A ->
+  infabs_depth_wl ({A ᵃʷ/ₜ X} Γ2' ⧺ Γ1') <= infabs_depth_wl (Γ2 ⧺ X ~ᵃ ⬒ ;ᵃ Γ1).
+Proof.
+  intros Γ2. induction Γ2; intros * Hwf Hsubst Hnotin Hmono;
+    dependent destruction Hwf; dependent destruction Hsubst;
+    simpl in *; eauto; try solve [exfalso; eauto];
+    try solve [eapply IHΓ2; eauto;
+      try solve [eapply a_mono_typ_strengthen_var; eauto];
+      try solve [eapply a_mono_typ_strengthen_mtvar with (b := □%abind); eauto];
+      try solve [eapply a_mono_typ_strengthen_mtvar with (b := ⬒); eauto];
+      try solve [eapply a_mono_typ_strengthen_stvar; eauto]].
+  - eapply worklist_split_etvar_det in x. destruct x. subst.
+    rewrite infabs_depth_wl_move_etvar_back with (Y := X); eauto.
+    eapply IHΓ2 with (Γ1 := X ~ᵃ ⬒ ;ᵃ Γ1) (A := A); eauto.
+    eapply a_wf_wwl_move_etvar_back; eauto.
+    eapply a_mono_typ_move_etvar_back; eauto.
+    eapply a_wf_wwl_tvar_notin_remaining in Hwf; eauto.
+  - eapply infabs_depth_work_subst_mono with (X := X) (w := w) in Hmono as Hle.
+    eapply IHΓ2 in Hsubst; eauto. lia.
+Qed.
+
+Lemma infabs_depth_wl_aworklist_subst' : forall Γ X A Γ1' Γ2',
+  ⊢ᵃʷ Γ -> X ~ ⬒ ∈ᵃ ⌊ Γ ⌋ᵃ ->
+  aworklist_subst Γ X A Γ1' Γ2' ->
+  ⌊ Γ ⌋ᵃ ᵗ⊢ᵃₘ A ->
+  X `notin` ftvar_in_typ A ->
+  infabs_depth_wl ({A ᵃʷ/ₜ X} Γ2' ⧺ Γ1') <= infabs_depth_wl Γ.
+Proof.
+  intros * Hwf Hbinds Hsubst Hmono Hnotin.
+  eapply awl_split_etvar in Hbinds as [Γ1 [Γ2 Hbinds]]. subst.
+  eapply infabs_depth_wl_aworklist_subst in Hsubst; eauto.
+Qed.
+
 Lemma a_wf_wl_red_decidable : forall me mj mt mtj ma maj ms mw ne Γ,
   ⊢ᵃʷₛ Γ ->
   exp_size_wl Γ ne -> ne < me ->
