a2f6deb7cbcae4ee20007c3531f0df4d60bde3e5
Shengyi Jiang <shengyi.jiang@outlook.com>
2024-10-13 11:12:33 +0800

    update _.v

--- proof/_.v
+++ proof/_.v
@@ -1,85 +1,101 @@
 (*      *)
 (* Base *)
 (*      *)
-Require Import uni.decl.prop_subtyping.
-Require Import uni.decl.prop_typing.
-Require Import uni.decl.prop_safety.
-Require Import uni.decl_worklist.prop_equiv.
-Require Import uni.algo_worklist.prop_soundness.
-Require Import uni.algo_worklist.prop_completeness.
-Require Import uni.algo_worklist.prop_decidability.
+Section Base.
 
-(* Prop of decl system *)
-Check uni.decl.prop_subtyping.d_sub_transitivity.
-Print Assumptions uni.decl.prop_subtyping.d_sub_transitivity.
-Check uni.decl.prop_typing.d_chk_inf_subsumption.
-Print Assumptions uni.decl.prop_typing.d_chk_inf_subsumption.
-Check uni.decl.prop_safety.d_chk_inf_elab_sound_f.
-Print Assumptions uni.decl.prop_safety.d_chk_inf_elab_sound_f.
+    Require Import uni.notations.
+    Require Import uni.decl.prop_subtyping.
+    Require Import uni.decl.prop_typing.
+    Require Import uni.decl.prop_safety.
+    Require Import uni.decl_worklist.prop_equiv.
+    Require Import uni.algo_worklist.prop_soundness.
+    Require Import uni.algo_worklist.prop_completeness.
+    Require Import uni.algo_worklist.prop_decidability.
 
-(* Prop of decl worklist system *)
-Check uni.decl_worklist.prop_equiv.d_wl_red_sound.
-Print Assumptions uni.decl_worklist.prop_equiv.d_wl_red_sound.
-Check uni.decl_worklist.prop_equiv.d_wl_red_complete.
-Print Assumptions uni.decl_worklist.prop_equiv.d_wl_red_complete.
+    (* Prop of decl system *)
+    Check uni.decl.prop_subtyping.d_sub_transitivity.
+    Print Assumptions uni.decl.prop_subtyping.d_sub_transitivity.
+    Check uni.decl.prop_typing.d_chk_inf_subsumption.
+    Print Assumptions uni.decl.prop_typing.d_chk_inf_subsumption.
+    Check uni.decl.prop_safety.d_chk_inf_elab_sound_f.
+    Print Assumptions uni.decl.prop_safety.d_chk_inf_elab_sound_f.
 
-(* Prop of algo worklist system *)
-Check uni.algo_worklist.prop_soundness.a_wl_red_soundness.
-Print Assumptions uni.algo_worklist.prop_soundness.a_wl_red_soundness.
-Check uni.algo_worklist.prop_completeness.a_wl_red_completeness.
-Print Assumptions uni.algo_worklist.prop_completeness.a_wl_red_completeness.
-Check uni.algo_worklist.prop_decidability.a_wf_wl_red_decidable.
-Print Assumptions uni.algo_worklist.prop_decidability.a_wf_wl_red_decidable.
+    (* Prop of decl worklist system *)
+    Check uni.decl_worklist.prop_equiv.d_wl_red_sound.
+    Print Assumptions uni.decl_worklist.prop_equiv.d_wl_red_sound.
+    Check uni.decl_worklist.prop_equiv.d_wl_red_complete.
+    Print Assumptions uni.decl_worklist.prop_equiv.d_wl_red_complete.
+
+    (* Prop of algo worklist system *)
+    Check uni.algo_worklist.prop_soundness.a_wl_red_soundness.
+    Print Assumptions uni.algo_worklist.prop_soundness.a_wl_red_soundness.
+    Check uni.algo_worklist.prop_completeness.a_wl_red_completeness.
+    Print Assumptions uni.algo_worklist.prop_completeness.a_wl_red_completeness.
+    Check uni.algo_worklist.prop_decidability.a_wf_wl_red_decidable.
+    Print Assumptions uni.algo_worklist.prop_decidability.a_wf_wl_red_decidable.
+
+End Base.
 
 
 (*                  *)
 (* Record Extension *)
 (*                  *)
-Require Import uni_rec.decl.prop_subtyping.
-Require Import uni_rec.decl.prop_typing.
-Require Import uni_rec.decl_worklist.prop_equiv.
-Require Import uni_rec.algo_worklist.prop_soundness.
-Require Import uni_rec.algo_worklist.prop_completeness.
+Section Rcd_Extension.
 
-(* Prop of decl system *)
-Check uni_rec.decl.prop_subtyping.d_sub_transitivity.
-Print Assumptions uni_rec.decl.prop_subtyping.d_sub_transitivity.
-Check uni_rec.decl.prop_typing.d_chk_inf_subsumption.
-Print Assumptions uni_rec.decl.prop_typing.d_chk_inf_subsumption.
+    Require Import uni_rec.notations.
+    Require Import uni_rec.decl.prop_subtyping.
+    Require Import uni_rec.decl.prop_typing.
+    Require Import uni_rec.decl_worklist.prop_equiv.
+    Require Import uni_rec.algo_worklist.prop_soundness.
+    Require Import uni_rec.algo_worklist.prop_completeness.
 
-(* Prop of decl worklist system *)
-Check uni_rec.decl_worklist.prop_equiv.d_wl_red_sound.
-Print Assumptions uni_rec.decl_worklist.prop_equiv.d_wl_red_sound.
-Check uni_rec.decl_worklist.prop_equiv.d_wl_red_complete.
-Print Assumptions uni_rec.decl_worklist.prop_equiv.d_wl_red_complete.
+    (* Prop of decl system *)
+    Check uni_rec.decl.prop_subtyping.d_sub_transitivity.
+    Print Assumptions uni_rec.decl.prop_subtyping.d_sub_transitivity.
+    Check uni_rec.decl.prop_typing.d_chk_inf_subsumption.
+    Print Assumptions uni_rec.decl.prop_typing.d_chk_inf_subsumption.
 
-(* Prop of algo worklist system *)
-Check uni_rec.algo_worklist.prop_soundness.a_wl_red_soundness.
-Print Assumptions uni_rec.algo_worklist.prop_soundness.a_wl_red_soundness.
-Check uni_rec.algo_worklist.prop_completeness.a_wl_red_completeness.
-Print Assumptions uni_rec.algo_worklist.prop_completeness.a_wl_red_completeness.
+    (* Prop of decl worklist system *)
+    Check uni_rec.decl_worklist.prop_equiv.d_wl_red_sound.
+    Print Assumptions uni_rec.decl_worklist.prop_equiv.d_wl_red_sound.
+    Check uni_rec.decl_worklist.prop_equiv.d_wl_red_complete.
+    Print Assumptions uni_rec.decl_worklist.prop_equiv.d_wl_red_complete.
+
+    (* Prop of algo worklist system *)
+    Check uni_rec.algo_worklist.prop_soundness.a_wl_red_soundness.
+    Print Assumptions uni_rec.algo_worklist.prop_soundness.a_wl_red_soundness.
+    Check uni_rec.algo_worklist.prop_completeness.a_wl_red_completeness.
+    Print Assumptions uni_rec.algo_worklist.prop_completeness.a_wl_red_completeness.
+    
+End Rcd_Extension.
 
 
 (*                                                      *)
 (* Record Extension and Instantiable Intersection/union *)
 (*                                                      *)
-Require Import uni_monoiu.decl.prop_subtyping.
-Require Import uni_monoiu.decl.prop_typing.
-Require Import uni_monoiu.decl_worklist.prop_equiv.
-Require Import uni_monoiu.algo_worklist.prop_soundness.
+Section Monoiu_Extension.
 
-(* Prop of decl system *)
-Check uni_monoiu.decl.prop_subtyping.d_sub_transitivity.
-Print Assumptions uni_monoiu.decl.prop_subtyping.d_sub_transitivity.
-Check uni_monoiu.decl.prop_typing.d_chk_inf_subsumption.
-Print Assumptions uni_monoiu.decl.prop_typing.d_chk_inf_subsumption.
+    Require Import uni_monoiu.notations.
+    Require Import uni_monoiu.decl.prop_subtyping.
+    Require Import uni_monoiu.decl.prop_typing.
+    Require Import uni_monoiu.decl_worklist.prop_equiv.
+    Require Import uni_monoiu.algo_worklist.prop_soundness.
 
-(* Prop of decl worklist system *)
-Check uni_monoiu.decl_worklist.prop_equiv.d_wl_red_sound.
-Print Assumptions uni_monoiu.decl_worklist.prop_equiv.d_wl_red_sound.
-Check uni_monoiu.decl_worklist.prop_equiv.d_wl_red_complete.
-Print Assumptions uni_monoiu.decl_worklist.prop_equiv.d_wl_red_complete.
+    (* Prop of decl system *)
+    Check uni_monoiu.decl.prop_subtyping.d_sub_transitivity.
+    Print Assumptions uni_monoiu.decl.prop_subtyping.d_sub_transitivity.
+    Check uni_monoiu.decl.prop_typing.d_chk_inf_subsumption.
+    Print Assumptions uni_monoiu.decl.prop_typing.d_chk_inf_subsumption.
 
-(* Prop of algo worklist system *)
-Check uni_monoiu.algo_worklist.prop_soundness.a_wl_red_soundness.
-Print Assumptions uni_monoiu.algo_worklist.prop_soundness.a_wl_red_soundness.
+    (* Prop of decl worklist system *)
+    Check uni_monoiu.decl_worklist.prop_equiv.d_wl_red_sound.
+    Print Assumptions uni_monoiu.decl_worklist.prop_equiv.d_wl_red_sound.
+    Check uni_monoiu.decl_worklist.prop_equiv.d_wl_red_complete.
+    Print Assumptions uni_monoiu.decl_worklist.prop_equiv.d_wl_red_complete.
+
+    (* Prop of algo worklist system *)
+    Check uni_monoiu.algo_worklist.prop_soundness.a_wl_red_soundness.
+    Print Assumptions uni_monoiu.algo_worklist.prop_soundness.a_wl_red_soundness.
+
+End Monoiu_Extension.
+
